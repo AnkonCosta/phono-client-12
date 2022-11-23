07460e777a2437235f630fd6566075fbde3e6e98
AnkonCosta <ankoncosta247@gmail.com>
2022-11-24 01:27:21 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAovyUd5sadWu5UH7_ev1lC4Kqmw_tbckE",
+  authDomain: "phono-1.firebaseapp.com",
+  projectId: "phono-1",
+  storageBucket: "phono-1.appspot.com",
+  messagingSenderId: "324158024659",
+  appId: "1:324158024659:web:36e1c1a03a712c8d0cdd26"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
