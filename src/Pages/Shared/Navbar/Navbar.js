fc23dbb6728e24f25c1df57422aa1cbf19799911
AnkonCosta <ankoncosta247@gmail.com>
2022-11-24 11:20:37 +0600

    added phones

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,16 +1,7 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { AuthContext } from "../../../contexts/AuthProvider";
 
 const Navbar = () => {
-  const { user, logOut } = useContext(AuthContext);
-
-  const handleLogOut = () => {
-    logOut()
-      .then(() => {})
-      .catch((err) => console.log(err));
-  };
-
   const menuItems = (
     <React.Fragment>
       <li>
@@ -21,6 +12,12 @@ const Navbar = () => {
       </li>
       <li>
         <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/reviews">Reviews</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
       </li>
     </React.Fragment>
   );
@@ -46,39 +43,19 @@ const Navbar = () => {
             </svg>
           </label>
           <ul
-            tabIndex={1}
+            tabIndex={0}
             className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
             {menuItems}
           </ul>
         </div>
         <Link to="/" className="btn btn-ghost normal-case text-xl">
-          Porto
+          PHONO
         </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">{menuItems}</ul>
       </div>
-      <label
-        htmlFor="dashboard-drawer"
-        tabIndex={2}
-        className="btn btn-ghost lg:hidden"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16"
-          />
-        </svg>
-      </label>
     </div>
   );
 };
